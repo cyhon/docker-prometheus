462d2b13287a26171a850574c84bbe5337cc219c
linhaitao <linhaitao@finogeeks.com>
2017-06-06 11:12:59 +0800

    分库

--- alertmanager/push.sh
+++
@@ -1,2 +0,0 @@
-docker build -t docker.finogeeks.club/monitor/alertmanager . || exit 1
-docker push docker.finogeeks.club/monitor/alertmanager
--- grafana/push.sh
+++
@@ -1,2 +0,0 @@
-docker build -t docker.finogeeks.club/monitor/grafana . || exit 1
-docker push docker.finogeeks.club/monitor/grafana
