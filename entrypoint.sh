0c005f8690079b6caa5f72e84ca030c7a5bab051
linhaitao <linhaitao@finogeeks.com>
2017-11-18 21:38:49 +0800

    slim image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/prometheus/prometheus --config.file=/prometheus/conf/${CONFIG_FILE} \
-                       --storage.tsdb.path=/prometheus/data \
-                       --web.console.libraries=/prometheus/console_libraries
-                       --web.console.templates=/prometheus/consoles
+/bin/prometheus --config.file=/prometheus/conf/${CONFIG_FILE} \
+                --storage.tsdb.path=/prometheus/data \
+                --web.console.libraries=/etc/prometheus/console_libraries \
+                --web.console.templates=/etc/prometheus/consoles
