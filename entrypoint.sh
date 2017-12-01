b49a9965e6f8bd8d951eb2c9804be4e738ed1c65
linhaitao <linhaitao@finogeeks.com>
2017-12-01 18:48:40 +0800

    临时解决方案：容器重启后PID相同导致文件锁异常

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# https://github.com/prometheus/prometheus/issues/2689
+rm -f /prometheus/data/lock
 /bin/prometheus --config.file=/prometheus/conf/${CONFIG_FILE} \
                 --storage.tsdb.path=/prometheus/data \
                 --web.console.libraries=/etc/prometheus/console_libraries \
