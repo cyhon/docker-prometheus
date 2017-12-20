0c0106a0e9859823061f867abb5826155074a241
linhaitao <linhaitao@finogeeks.com>
2017-12-20 17:52:31 +0800

    [skip ci]merge yaml conf

---
+++ modules/merge.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker pull docker.finogeeks.club/tools/yaml-merge
+docker run -it --rm -v `pwd`:/app docker.finogeeks.club/tools/yaml-merge yaml_merge -i global.yml mvp.yml business.yml docker.yml infrastructure.yml prober.yml -o finochat.yml
