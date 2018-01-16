3ec31025ced79ba4183c7c3e5216381c1ccd534e
luozhaofeng <luozhaofeng@finogeeks.com>
2018-01-16 17:37:05 +0800

    udpate es monitor

--- modules/finogeeks.club.sh
+++ modules/finogeeks.club.sh
@@ -2,6 +2,6 @@
 cd $(dirname $0)
 
 docker pull docker.finogeeks.club/tools/yaml-merge
-docker run -it --rm -v `pwd`:/app docker.finogeeks.club/tools/yaml-merge yaml_merge -i global.yml staff-homeserver.yml mvp.yml business.yml docker.yml mongodb.yml prober.yml -o finogeeks.club.yml
+docker run -it --rm -v `pwd`:/app docker.finogeeks.club/tools/yaml-merge yaml_merge -i global.yml staff-homeserver.yml mvp.yml business.yml docker.yml mongodb.yml prober.yml elasticsearch.yml -o finogeeks.club.yml
 
 mv finogeeks.club.yml ../conf/
