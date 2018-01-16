f795e12a02d089bf5f784e75d3d2c2e59891132a
linhaitao <linhaitao@finogeeks.com>
2018-01-16 14:01:14 +0800

    update

---
+++ modules/finogeeks.com.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd $(dirname $0)
+
+docker pull docker.finogeeks.club/tools/yaml-merge
+docker run -it --rm -v `pwd`:/app docker.finogeeks.club/tools/yaml-merge yaml_merge -i global.yml staff-homeserver.yml mvp.yml docker.yml mongodb.yml pg-website.yml prober.yml -o finogeeks.com.yml
+
+mv finogeeks.com.yml ../conf/
