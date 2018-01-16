9c2d10bec73cf2e63b9ff28137f3ad0eef977bad
linhaitao <linhaitao@finogeeks.com>
2018-01-16 10:41:31 +0800

    update finogeeks.club.yml

---
+++ modules/finogeeks.club.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd $(dirname $0)
+
+docker pull docker.finogeeks.club/tools/yaml-merge
+docker run -it --rm -v `pwd`:/app docker.finogeeks.club/tools/yaml-merge yaml_merge -i global.yml staff-homeserver.yml mvp.yml business.yml docker.yml mongodb.yml prober.yml -o finogeeks.club.yml
+
+mv finogeeks.club.yml ../conf/
--- modules/merge.sh
+++ modules/merge.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+cd $(dirname $0)
+
 docker pull docker.finogeeks.club/tools/yaml-merge
 docker run -it --rm -v `pwd`:/app docker.finogeeks.club/tools/yaml-merge yaml_merge -i global.yml mvp.yml business.yml docker.yml infrastructure.yml prober.yml -o finochat.yml
